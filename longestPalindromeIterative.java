bfbcb55ce56737e9cc60a867052323187d3436dc
Harshada Gundecha <63367010+HarshadaG06@users.noreply.github.com>
2023-04-17 17:55:28 +0530

    Modifications

--- longestPalindromeIterative.java
+++ longestPalindromeIterative.java
@@ -1,3 +1,21 @@
+/*
+14-04-2023
+https://leetcode.com/problems/longest-palindromic-subsequence/
+
+Given a string s, find the longest palindromic subsequence's length in s.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without
+changing the order of the remaining elements.
+
+Examples:
+Input: s = "bbbab"
+Output: 4
+Explanation: One possible longest palindromic subsequence is "bbbb".
+
+Input: s = "cbbd"
+Output: 2
+Explanation: One possible longest palindromic subsequence is "bb".
+*/
 import java.util.*;
 public class longestPalindromeIterative {
     public static int longestPalindromeSubseq(String s) {
